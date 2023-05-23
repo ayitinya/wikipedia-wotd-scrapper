f4390b1c9acc7d5ca1d299048eb970969c4c750f
Rudy Ayitinya <contact@ayitinya.me>
2023-05-23 14:43:43 +0100

    removes store data to file
    vercel doesn't support writes to their filesystem

--- api/main.py
+++ api/main.py
@@ -2,7 +2,6 @@ from http.server import BaseHTTPRequestHandler
 
 
 import json
-from typing import Dict
 from datetime import datetime
 
 
@@ -19,51 +18,21 @@ def get_wotd():
 
     if results is not None:
         json_data = json.dumps({"wotd": results.text, "timestamp": str(datetime.now())})
-        with open("wotd.json", "w") as f:
-            f.write(json_data)
         return json_data
     else:
         return None
 
 class handler(BaseHTTPRequestHandler):
- 
     def do_GET(self):
-        try:
-            with open("wotd.json", "r") as f:
-                json_data = f.read()
-                
-                self.send_response(200)
-                self.send_header('Content-type','application/json')
-                self.end_headers()
-                
-                parse_json: Dict[str, str]  = json.loads(json_data)
-                current_time = datetime.now()
-
-                delta = current_time - datetime.strptime(parse_json["timestamp"], "%Y-%m-%d %H:%M:%S.%f")
-                
-                if delta.days > 0:
-                    json_data = get_wotd()
-                    if json_data is not None:
-                        
-                        self.wfile.write(json_data.encode('utf-8'))
-                        return
-                else:
-                    self.wfile.write(json_data.encode('utf-8'))
-                    return
-                    
-                    
-        except FileNotFoundError:
-            json_data = get_wotd()
-            if json_data is not None:
+        json_data = get_wotd()
+        if json_data is not None:
                 
                 self.send_response(200)
                 self.send_header('Content-type','application/json')
                 self.end_headers()
                 self.wfile.write(json_data.encode('utf-8'))
                 return
-            else:
-                self.send_response(404)
-                return
-        except:
-            self.send_response(500)
+        else:
+            self.send_response(404)
             return
+ 
