f514c92723a44e1dac26a96628cdf12e3999e65a
Rudy Ayitinya <contact@ayitinya.me>
2023-05-23 14:16:06 +0100

    adds time check for existing data

--- api/main.py
+++ api/main.py
@@ -2,6 +2,10 @@ from http.server import BaseHTTPRequestHandler
 
 from get_wotd import get_wotd
 
+import json
+from typing import Dict
+from datetime import datetime
+
 
 class handler(BaseHTTPRequestHandler):
  
@@ -9,9 +13,31 @@ class handler(BaseHTTPRequestHandler):
         try:
             with open("wotd.json", "r") as f:
                 json_data = f.read()
+                
+                self.send_response(200)
+                self.send_header('Content-type','application/json')
+                self.end_headers()
+                
+                parse_json: Dict[str, str]  = json.loads(json_data)
+                current_time = datetime.now()
+
+                delta = current_time - datetime.strptime(parse_json["timestamp"], "%Y-%m-%dT%H:%M:%S.%f")
+                
+                if delta.days > 0:
+                    json_data = get_wotd()
+                    if json_data is not None:
+                        
+                        self.wfile.write(json_data.encode('utf-8'))
+                        return
+                else:
+                    self.wfile.write(json_data.encode('utf-8'))
+                    return
+                    
+                    
         except FileNotFoundError:
             json_data = get_wotd()
             if json_data is not None:
+                
                 self.send_response(200)
                 self.send_header('Content-type','application/json')
                 self.end_headers()
