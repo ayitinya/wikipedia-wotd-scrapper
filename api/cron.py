ec86686b5828b3616af6066b53aed502266293f0
Rudy Ayitinya <contact@ayitinya.me>
2023-05-23 14:26:26 +0100

    removes import of get_wotd due to import error on vercel

--- api/cron.py
+++ api/cron.py
@@ -1,6 +1,26 @@
 from http.server import BaseHTTPRequestHandler
 
-from get_wotd import get_wotd
+import json
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+import requests
+
+def get_wotd():
+    URL: str = "https://en.wiktionary.org/wiki/Wiktionary:Word_of_the_day"
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    results = soup.find(id="WOTD-rss-title")
+
+    if results is not None:
+        json_data = json.dumps({"wotd": results.text, "timestamp": str(datetime.now())})
+        with open("wotd.json", "w") as f:
+            f.write(json_data)
+        return json_data
+    else:
+        return None
 
 
 class handler(BaseHTTPRequestHandler):
